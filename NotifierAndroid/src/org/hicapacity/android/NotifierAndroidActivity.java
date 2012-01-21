0f9b97aade0b74c599358075f5eb4f20bc00a6f8
Jason Axelson <jason.axelson@gmail.com>
2012-01-20 18:39:10 -1000

    Start Android Project

---
+++ NotifierAndroid/src/org/hicapacity/android/NotifierAndroidActivity.java
@@ -0,0 +1,13 @@
+package org.hicapacity.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NotifierAndroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
