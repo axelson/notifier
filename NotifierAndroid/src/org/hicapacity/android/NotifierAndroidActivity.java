6a549d617e89d839602e3f417a472a8652717b73
Jason Axelson <jason.axelson@gmail.com>
2012-01-20 19:17:16 -1000

    Handle button press

--- NotifierAndroid/src/org/hicapacity/android/NotifierAndroidActivity.java
+++ NotifierAndroid/src/org/hicapacity/android/NotifierAndroidActivity.java
@@ -2,12 +2,28 @@ package org.hicapacity.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class NotifierAndroidActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+  private Button but;
+
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+
+    but = (Button) this.findViewById(R.id.button1);
+    but.setText("New text");
+    but.setOnClickListener(new OnClickListener() {
+
+      @Override
+      public void onClick(View v) {
+        System.out.println("Button pressed!");
+        but.setText("newer text");
+      }
+    });
+  }
 }
