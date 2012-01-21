30b337573abb4ed613ecadf11376ae4b62765824
Jason Axelson <jason.axelson@gmail.com>
2012-01-20 19:59:27 -1000

    Create event detail activity
    Nothing uses it yet

---
+++ NotifierAndroid/src/org/hicapacity/android/EventDetailActivity.java
@@ -0,0 +1,17 @@
+package org.hicapacity.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author Jason Axelson
+ *
+ */
+public class EventDetailActivity extends Activity {
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.event_detail);
+  }
+}
