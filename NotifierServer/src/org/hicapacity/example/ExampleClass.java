b164ff74efae7be3853f4694eaa1662b42e4b705
Jason Axelson <jason.axelson@gmail.com>
2012-01-20 18:31:06 -1000

    Initial java server project

---
+++ NotifierServer/src/org/hicapacity/example/ExampleClass.java
@@ -0,0 +1,17 @@
+package org.hicapacity.example;
+
+/**
+ * @author Jason Axelson
+ *
+ */
+public class ExampleClass {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    System.out.println("Hello CityCamp Honolulu!");
+  }
+
+}
