ed8ba7358a993a980e1834a44fbe24d7a20d81ae
Jason Axelson <jason.axelson@gmail.com>
2012-01-20 19:25:59 -1000

    simple notification

--- NotifierAndroid/src/org/hicapacity/android/NotifierAndroidActivity.java
+++ NotifierAndroid/src/org/hicapacity/android/NotifierAndroidActivity.java
@@ -1,6 +1,11 @@
 package org.hicapacity.android;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -8,6 +13,8 @@ import android.widget.Button;
 
 public class NotifierAndroidActivity extends Activity {
   private Button but;
+  protected Activity activity = this;
+  private static final int HELLO_ID = 1;
 
   /** Called when the activity is first created. */
   @Override
@@ -23,6 +30,28 @@ public class NotifierAndroidActivity extends Activity {
       public void onClick(View v) {
         System.out.println("Button pressed!");
         but.setText("newer text");
+
+        String ns = Context.NOTIFICATION_SERVICE;
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(ns);
+
+        // TODO: use an official icon from Aaron
+        int icon = R.drawable.icon;
+        CharSequence tickerText = "Hello";
+        long when = System.currentTimeMillis();
+
+        Notification notification = new Notification(icon, tickerText, when);
+
+        Context context = getApplicationContext();
+        CharSequence contentTitle = "My notification";
+        CharSequence contentText = "Hello World!";
+        Intent notificationIntent = new Intent(NotifierAndroidActivity.this,
+            NotifierAndroidActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(NotifierAndroidActivity.this,
+            0, notificationIntent, 0);
+
+        notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
+
+        mNotificationManager.notify(HELLO_ID, notification);
       }
     });
   }
